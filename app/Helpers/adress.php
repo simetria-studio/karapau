6346ad40fd3caf33b702e9bee0d9dd8e6e60a274
felipe cruz <felipephplow@gmail.com>
2021-08-18 11:00:54 -0300

    dsa

---
+++ app/Helpers/adress.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\AdressBuyer;
+use App\Models\ShippingTax;
+use Darryldecode\Cart\Cart;
+
+
+
+function getValue($portoId)
+{
+
+
+    $adress = App\Models\AdressBuyer::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->first();
+    $porto = App\Models\Porto::find($portoId);
+
+    $url =  Illuminate\Support\Facades\Http::get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=$porto->latitude,$porto->longitude&destinations=$adress->latitude,$adress->longitude&key=AIzaSyCcTnukB7zVZVr3T-Pk6-Lptswge0BDOXg");
+
+
+    $collection = collect(json_decode($url, true));
+
+    $dist = $collection['rows'][0]['elements'][0]['distance']['text'];
+    $distF = (float)$dist;
+    $distC = $distF;
+    $mnH = $distC * 1.609;
+
+    return $mnH;
+
+
+}
